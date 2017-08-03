423669b5373f13b1bf2a63b9191e9ca2bba1dcf0
davidmcdeavitt <davidmcdeavitt@gmail.com>
2017-08-03 16:45:35 +1000

    created listeners for button and master function
    git it

---
+++ app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+
+  var savedDealsButton = document.querySelector('#savedDealsButton');
+  var preferredDealsButton = document.querySelector('#preferredDealsButton');
+  }
